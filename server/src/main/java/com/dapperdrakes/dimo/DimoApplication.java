909ad90c311146802eb95090877d30a9e06db46b
Balaji A <balaji.arumugam@thoughtworks.com>
2020-01-27 13:55:07 +0530

    [Balaji] add spring project to server folder

---
+++ server/src/main/java/com/dapperdrakes/dimo/DimoApplication.java
@@ -0,0 +1,13 @@
+package com.dapperdrakes.dimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimoApplication.class, args);
+	}
+
+}
