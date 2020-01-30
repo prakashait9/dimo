5d3c00a00f7682d2b7880d30fcc0d2801ca39355
Ray Mathew <ray.mathew@thoughtworks.com>
2020-01-30 09:30:43 +0530

    reverted changes made to css of active tab

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -109,15 +109,15 @@ export class LoginComponent implements OnInit {
     console.log(tabName);
     if (tabName == 'login') {
       this.loginTab.style.display = 'block';
-      this.signUpLink.classList.remove('active');
+      this.signUpLink.classList.add('active');
       this.signUpTab.style.display = 'none';
-      this.loginLink.classList.add('active');
+      this.loginLink.classList.remove('active');
     }
     else {
       this.loginTab.style.display = 'none';
-      this.signUpLink.classList.add('active');
+      this.signUpLink.classList.remove('active');
       this.signUpTab.style.display = 'block';
-      this.loginLink.classList.remove('active');
+      this.loginLink.classList.add('active');
     }
     return false;
   }
