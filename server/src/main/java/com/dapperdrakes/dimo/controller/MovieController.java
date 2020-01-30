ae8cd45713997f39c006d428c3e481cedb779adf
Sujit Kumar <sujit.kumar@thoughtworks.com>
2020-01-30 16:04:13 +0530

    Fixed imports

--- server/src/main/java/com/dapperdrakes/dimo/controller/MovieController.java
+++ server/src/main/java/com/dapperdrakes/dimo/controller/MovieController.java
@@ -6,15 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.dapperdrakes.dimo.model.GenericResponse;
 import com.dapperdrakes.dimo.service.MovieService;
-import com.dapperdrakes.dimo.util.GenericResponse;
 
 @RestController
 @RequestMapping("/api")
